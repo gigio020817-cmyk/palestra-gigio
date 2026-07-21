17fed17de2c85deaddec08fdd06a906acd21b188
gigio020817-cmyk <gigio020817-cmyk@users.noreply.github.com>
2026-07-21 20:12:46 +0000

    Schede per-profilo: owner su schede predefinite (Gigio s1-s3, Erjola s4-s6), filtro home per profilo attivo, migrazione gg_seed_v4, owner sulle nuove schede, cache sw v24

--- gym-app-telefono/sw.js
+++ gym-app-telefono/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gigio-gym-v23';
+const CACHE = 'gigio-gym-v24';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './goku.jpg'];
 
 self.addEventListener('install', (e) => {
