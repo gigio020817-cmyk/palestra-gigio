a88bba6e52ed50c34b61450afacbbbf1033e6c8d
Gigio <gigio020817@gmail.com>
2026-07-21 20:38:28 +0000

    Immagini esercizi offline (2 fotogrammi con movimento) + vista 'come si fa' dalla scheda senza avviare; niente zoom col doppio tap; durata stimata e schede adattate a ~30 min (Gigio e Erjola); recuperi a 75s; migrazione gg_seed_v5; cache sw v26

--- gym-app-telefono/sw.js
+++ gym-app-telefono/sw.js
@@ -1,11 +1,67 @@
-const CACHE = 'gigio-gym-v25';
+const CACHE = 'gigio-gym-v26';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './goku.jpg'];
+// immagini esercizi impacchettate: precaricate così funzionano OFFLINE (in palestra)
+const IMG_SHELL = [
+  './img/exercises/Barbell_Glute_Bridge/0.jpg',
+  './img/exercises/Barbell_Glute_Bridge/1.jpg',
+  './img/exercises/Barbell_Hip_Thrust/0.jpg',
+  './img/exercises/Barbell_Hip_Thrust/1.jpg',
+  './img/exercises/Barbell_Shoulder_Press/0.jpg',
+  './img/exercises/Barbell_Shoulder_Press/1.jpg',
+  './img/exercises/Barbell_Shrug/0.jpg',
+  './img/exercises/Barbell_Shrug/1.jpg',
+  './img/exercises/Butterfly/0.jpg',
+  './img/exercises/Butterfly/1.jpg',
+  './img/exercises/Close-Grip_Front_Lat_Pulldown/0.jpg',
+  './img/exercises/Close-Grip_Front_Lat_Pulldown/1.jpg',
+  './img/exercises/Crunches/0.jpg',
+  './img/exercises/Crunches/1.jpg',
+  './img/exercises/Dumbbell_Bench_Press/0.jpg',
+  './img/exercises/Dumbbell_Bench_Press/1.jpg',
+  './img/exercises/Dumbbell_Bicep_Curl/0.jpg',
+  './img/exercises/Dumbbell_Bicep_Curl/1.jpg',
+  './img/exercises/Dumbbell_Lunges/0.jpg',
+  './img/exercises/Dumbbell_Lunges/1.jpg',
+  './img/exercises/EZ-Bar_Curl/0.jpg',
+  './img/exercises/EZ-Bar_Curl/1.jpg',
+  './img/exercises/Front_Barbell_Squat/0.jpg',
+  './img/exercises/Front_Barbell_Squat/1.jpg',
+  './img/exercises/Goblet_Squat/0.jpg',
+  './img/exercises/Goblet_Squat/1.jpg',
+  './img/exercises/Hammer_Curls/0.jpg',
+  './img/exercises/Hammer_Curls/1.jpg',
+  './img/exercises/Machine_Bench_Press/0.jpg',
+  './img/exercises/Machine_Bench_Press/1.jpg',
+  './img/exercises/One-Arm_Dumbbell_Row/0.jpg',
+  './img/exercises/One-Arm_Dumbbell_Row/1.jpg',
+  './img/exercises/One-Arm_Kettlebell_Row/0.jpg',
+  './img/exercises/One-Arm_Kettlebell_Row/1.jpg',
+  './img/exercises/Plank/0.jpg',
+  './img/exercises/Plank/1.jpg',
+  './img/exercises/Romanian_Deadlift/0.jpg',
+  './img/exercises/Romanian_Deadlift/1.jpg',
+  './img/exercises/Seated_Bent-Over_Rear_Delt_Raise/0.jpg',
+  './img/exercises/Seated_Bent-Over_Rear_Delt_Raise/1.jpg',
+  './img/exercises/Seated_Dumbbell_Press/0.jpg',
+  './img/exercises/Seated_Dumbbell_Press/1.jpg',
+  './img/exercises/Seated_Triceps_Press/0.jpg',
+  './img/exercises/Seated_Triceps_Press/1.jpg',
+  './img/exercises/Side_Lateral_Raise/0.jpg',
+  './img/exercises/Side_Lateral_Raise/1.jpg',
+  './img/exercises/Standing_Calf_Raises/0.jpg',
+  './img/exercises/Standing_Calf_Raises/1.jpg',
+  './img/exercises/Underhand_Cable_Pulldowns/0.jpg',
+  './img/exercises/Underhand_Cable_Pulldowns/1.jpg',
+  './img/exercises/Wide-Grip_Lat_Pulldown/0.jpg',
+  './img/exercises/Wide-Grip_Lat_Pulldown/1.jpg',
+];
+const PRECACHE = SHELL.concat(IMG_SHELL);
 
 self.addEventListener('install', (e) => {
   // allSettled: se un singolo file non si scarica, l'installazione NON fallisce tutta
   e.waitUntil(
     caches.open(CACHE)
-      .then((c) => Promise.allSettled(SHELL.map((u) => c.add(u))))
+      .then((c) => Promise.allSettled(PRECACHE.map((u) => c.add(u))))
       .then(() => self.skipWaiting())
   );
 });
@@ -38,7 +94,6 @@ self.addEventListener('fetch', (e) => {
   }
 
   // Altre risorse (immagini, ecc.): prima la copia salvata, poi internet.
-  // Non restituisce MAI una risposta vuota (che causava l'errore del service worker).
   e.respondWith(
     caches.match(req).then((cached) => {
       if (cached) return cached;
