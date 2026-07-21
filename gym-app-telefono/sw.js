140c4130ba62f0bb8d2e9428b2fcec5914f04886
Gigio <gigio020817@gmail.com>
2026-07-21 20:18:59 +0000

    Allenamento visibile a tutti i profili (non solo Gigio): Erjola ora vede le sue schede; cache sw v25

--- gym-app-telefono/sw.js
+++ gym-app-telefono/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gigio-gym-v24';
+const CACHE = 'gigio-gym-v25';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './goku.jpg'];
 
 self.addEventListener('install', (e) => {
